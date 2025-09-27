6b22ead64f37dcf9673b79db3dc24540958d75fa
Nathan Douziech <nathan.douziech@gmail.com>
2025-09-27 18:19:30 +0200

    Initial Commit

---
+++ anxietyapp/anxietyappApp.swift
@@ -0,0 +1,17 @@
+//
+//  anxietyappApp.swift
+//  anxietyapp
+//
+//  Created by Nathan Douziech on 27/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct anxietyappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
